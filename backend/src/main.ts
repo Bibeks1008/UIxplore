297cdf2a50f3ebd71139ba4b22a2c4470ed5f63a
Bibeks1008 <bibekshrestha1008@gmail.com>
2025-01-06 13:52:55 +0545

    Enabled cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,31 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const whitelist = ['http://localhost:5173', 'https://uixplore.netlify.app'];
+
+  // Enable CORS with dynamic origin handling
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        // Allow requests without an origin (e.g., server-to-server communication)
+        callback(null, true);
+        return;
+      }
+      if (whitelist.includes(origin)) {
+        console.log('Allowed CORS for:', origin);
+        callback(null, true);
+      } else {
+        console.log('Blocked CORS for:', origin);
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
